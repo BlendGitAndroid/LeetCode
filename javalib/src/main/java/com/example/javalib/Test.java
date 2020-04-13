29463f67dba14390f372fcbb89125f18a561959e
Xu Hai <hxu15@best-inc.com>
2020-04-13 20:52:25 +0800

    青蛙跳台阶问题

--- javalib/src/main/java/com/example/javalib/Test.java
+++ javalib/src/main/java/com/example/javalib/Test.java
@@ -3,7 +3,7 @@ package com.example.javalib;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(new Fibonacci().fib(2));
+        System.out.println(new FrogJumps().numWays(4));
     }
 
 }
