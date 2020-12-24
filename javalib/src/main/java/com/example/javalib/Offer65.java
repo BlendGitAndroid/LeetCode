d2977a5149afbfb5986ba2275fe8bb3aca1bcadf
BlendAndroid <1059152860@qq.com>
2020-12-24 20:00:54 +0800

    不用加减乘除做加法

--- javalib/src/main/java/com/example/javalib/Offer65.java
+++ javalib/src/main/java/com/example/javalib/Offer65.java
@@ -5,11 +5,12 @@ package com.example.javalib;
  * 文中说不使用运算符进行两个整数的加法计算，所以在这里我们就用把这两个整数当初二进制来进行加法计算。
  * a或b为无进位和
  * a与b为进位，然后使用左移1位<<1，来表示左进一位
+ * 当没有进位的时候，表示已经完成了加法计算
  */
 class Offer65 {
 
     public static void main(String[] args) {
-        System.out.println(new Offer65().add(1,2));
+        System.out.println(new Offer65().add(1, 2));
     }
 
     /**
@@ -29,6 +30,18 @@ class Offer65 {
             a = a ^ b;  //在计算非进位，a = 非进位
             b = c;  //将进位赋值给b
         }
+        return a;
+    }
+
+    public int add1(int a, int b) {
+        int sum;
+        int carry;
+        do {
+            sum = a ^ b;    //不考虑进位
+            carry = (a & b) << 1;   //考虑进位
+            a = sum;
+            b = carry;
+        } while (b != 0);
         return a;
     }
 }
